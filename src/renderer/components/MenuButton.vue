be9bcf56f6349292ef45c07e35068422c936212f
Daglesia <daglesia.gaming@gmail.com>
2023-02-25 19:08:45 +0100

    improved button look + transitions

--- src/renderer/components/MenuButton.vue
+++ src/renderer/components/MenuButton.vue
@@ -1,10 +1,11 @@
 <template>
-    <button @click="onClick()" class="menu-button">
+    <button @click="onClick()" class="menu-button" :class="{animate: animated}">
         {{ label }}
     </button>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 defineProps({
   label: {
     type: String,
@@ -12,37 +13,17 @@ defineProps({
   },
 });
 
+const animated = ref(false);
+
 const onClick = () => {
+    animated.value = true;
+      setTimeout(() => {
+        animated.value = false;
+      }, 500);
     window.electronAPI.sendMessage('Hello from App.vuddde!');
 }
 </script>
 
 <style lang="scss" scoped>
-
-%button-shadow {
-    --color-shadow: var(--color-primary);
-
-    -webkit-box-shadow: inset 0px 0px 100px -34px var(--color-shadow);
-    -moz-box-shadow: inset 0px 0px 100px -34px var(--color-shadow);
-    box-shadow: inset 0px 0px 100px -34px var(--color-shadow);
-}
-
-p {
-    font-family: var(--font-button);
-    text-shadow: 2px 2px 4px var(--color-secondary);
-    font-size: 1.5em;
-}
-
-button {
-    @extend %button-shadow;
-    width: 100%;
-    height: 100%;
-    border-radius: 1em;
-    background-color: var(--color-secondary);
-    color: var(--color-accent);
-
-    font-family: var(--font-button);
-    text-shadow: 2px 2px 4px var(--color-secondary);
-    font-size: 1.5em;
-    }
+@use '../styles/buttons.scss';
 </style>
